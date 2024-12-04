c2ccf044c0f6b648319dbadf5d06ee5f4309748a
SeironSpecies <tuphiaworld@gmail.com>
2024-12-04 16:29:58 -0500

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -56,7 +56,7 @@ let options = {
         itemOrder: "asc",
 
         filterColumn: 'Design Type',
-        searchFilterParams: ['ID', 'Owner', 'Designer', 'Artist', 'Rarity', 'Codename', 'Traits', 'Secondary Species'],
+        searchFilterParams: ['ID', 'Owner', 'Designer', 'Artist', 'Rarity', 'Codename', 'Traits', 'Secondary Species', 'notes'],
         fauxFolderColumn: 'Species',
 
     },
