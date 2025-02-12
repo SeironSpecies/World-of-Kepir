bd79b3779bfa67e00aa67e6eb1dd97b8bd6c1cdc
SeironSpecies <tuphiaworld@gmail.com>
2025-02-12 13:11:31 -0500

    Update charadex.js

--- styles/charadex.js
+++ styles/charadex.js
@@ -502,91 +502,7 @@ const masterlist = async (options) => {
 };
 
 
-/* ==================================================================== */
-/* Egg Masterlist Only
-======================================================================= */
-const eggMasterlist = async (options) => {
 
-    // Sort through options
-    const charadexInfo = optionSorter(options);
-
-    // Grab the sheet
-    let sheetArray = await fetchSheet(charadexInfo.sheetPage);
-
-    // Grab all our url info
-    let cardKey = Object.keys(sheetArray[0])[3];
-    let cardKeyAlt = Object.keys(sheetArray[0])[0];
-
-    let preParam = urlParamFix(cardKey, charadexInfo.fauxFolderColumn);
-
-    // Create faux folders
-    // Filter through array based on folders
-    if (charadexInfo.fauxFolderColumn) sheetArray = fauxFolderButtons(sheetArray, charadexInfo.fauxFolderColumn);
-
-    // Reverse based on preference
-    charadexInfo.itemOrder == 'asc' ? sheetArray.reverse() : '';
-
-    // Add card links to the remaining array
-    for (var i in sheetArray) { 
-        sheetArray[i].cardlink = baseURL + preParam + sheetArray[i][cardKey]; 
-        sheetArray[i].cardlinkalt = baseURL + urlParamFix(cardKeyAlt, charadexInfo.fauxFolderColumn) + sheetArray[i][Object.keys(sheetArray[0])[0]]; 
-    }
-
-    // Decide if the url points to profile or entire gallery
-    if (urlParams.has(cardKey) || urlParams.has(cardKeyAlt)) {
-
-        // Filter out the right card
-        let currCardKey = urlParams.has(cardKey) ? cardKey : cardKeyAlt;
-        let singleCard = sheetArray.filter((i) => (i[currCardKey] === urlParams.get(currCardKey)))[0];
-
-        // Grab the log sheet and render log
-        let logArray = await fetchSheet(charadexInfo.logSheetPage);
-        getLog(logArray, singleCard);
-
-        // List.js options
-        let itemOptions = {
-            valueNames: sheetArrayKeys(sheetArray),
-            item: 'charadex-card',
-        };
-
-        // Render the prev/next links on profiles
-        prevNextLinks(sheetArray, baseURL, preParam, urlParams, currCardKey, cardKey);
-
-        // Render card
-        let charadexItem = new List("charadex-gallery", itemOptions, singleCard);
-
-
-    } else {
-
-        // Show pagination
-        showPagination(sheetArray, charadexInfo.itemAmount);
-
-        // Create the Gallery
-        let galleryOptions = {
-            item: 'charadex-entries',
-            valueNames: sheetArrayKeys(sheetArray),
-            searchColumns: charadexInfo.searchFilterParams,
-            page: charadexInfo.itemAmount,
-            pagination: [{
-                innerWindow: 1,
-                left: 1,
-                right: 1,
-                item: `<li class='page-item'><a class='page page-link'></a></li>`,
-                paginationClass: 'pagination-top',
-            }],
-        };
-
-        // Render Gallery
-        let charadex = new List('charadex-gallery', galleryOptions, sheetArray);
-
-        // Make filters workie
-        charadexFilterSelect(charadex, sheetArray, charadexInfo.filterColumn);
-        charadexSearch(charadex, charadexInfo.searchFilterParams);
-
-
-    }
-
-};
 
 /* ==================================================================== */
 /* Inventories
@@ -821,37 +737,6 @@ const frontPage = (options) => {
             }
         }
     }; addDesigns();
- // egg Designs
-    let addDesigns = async () => {
-        if ($("#design-gallery").length != 0) {
-            if (charadexInfo.numOfDesigns != 0) {
-
-                // Grab dah sheet
-                let designs = await fetchSheet(charadexInfo.eggMasterlistSheetPage);
-
-                // Filter out any MYO slots, reverse and pull the first 4
-                let selectDesigns = designs.filter((i) => { return i.designtype != 'MYO Slot' }).reverse().slice(0, charadexInfo.numOfDesigns);
-
-                // Add cardlink
-                let cardKey = Object.keys(selectDesigns[0])[0];
-                for (var i in selectDesigns) { selectDesigns[i].cardlink = folderURL + "eggMasterlist.html?" + cardKey + "=" + selectDesigns[i][cardKey]; }
-
-                // Nyoom
-                let galleryOptions = {
-                    item: 'design-item',
-                    valueNames: sheetArrayKeys(selectDesigns),
-                };
-
-                // Render Gallery
-                let charadex = new List('design-gallery', galleryOptions, selectDesigns);
-
-            } else {
-                $("#design-gallery").hide();
-            }
-        }
-    }; addDesigns();
-
-}; 
 
 }; 
 
